253f512406447421a2f6c74bf9ff623a6dd2726a
kouki.taniguchi <kouki.taniguchi>
2019-07-16 17:56:34 +0900

    add upload to image

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ApplicationRecord
+  validates :title, length: { maximum: 100 }
+  validates :text, length: { maximum: 500 }
+
   mount_uploader :image, ImageUploader
+  
   def self.search(word)
-    word.present? ? Product.where(title: word) : Product.all
+    word.present? ? Product.where("title LIKE ?", "%#{word}%") : Product.all
   end
 end
